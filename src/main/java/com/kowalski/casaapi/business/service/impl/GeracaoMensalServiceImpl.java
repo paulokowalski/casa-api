210d7b2ea92070b6383a2cd89ccd5bb38985876f
Paulo <paulokowalski@gmail.com>
2025-07-11 17:51:00 -0300

    implementando calculo de geracao diaria

--- src/main/java/com/kowalski/casaapi/business/service/impl/GeracaoMensalServiceImpl.java
+++ src/main/java/com/kowalski/casaapi/business/service/impl/GeracaoMensalServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kowalski.casaapi.business.service.impl;
 
+import com.kowalski.casaapi.business.model.Geracao;
 import com.kowalski.casaapi.business.model.GeracaoMensal;
 import com.kowalski.casaapi.business.repository.GeracaoMensalRepository;
 import com.kowalski.casaapi.business.service.GeracaoMensalService;
@@ -13,10 +14,8 @@ public class GeracaoMensalServiceImpl implements GeracaoMensalService {
     private final GeracaoMensalRepository geracaoMensalRepository;
 
     @Override
-    public GeracaoMensal salvar(GeracaoMensal geracaoMensal) {
-        return geracaoMensalRepository.findByMes(geracaoMensal.getMes()).map(geracao -> {
-            geracao.setGeracao(geracao.getGeracao() + geracaoMensal.getGeracao());
-            return geracaoMensalRepository.save(geracao);
-        }).orElseGet(() -> geracaoMensalRepository.save(geracaoMensal));
+    //TODO: Implementar o cálculo mensal
+    public GeracaoMensal salvar(Geracao geracao) {
+        return null;
     }
 }
