4db293d187493872c147a7977c8dfe37333aa5b9
Paulo <paulokowalski@gmail.com>
2025-07-20 08:49:03 -0300

    criando notificacaoController

--- src/main/java/com/kowalski/casaapi/api/v1/controller/NotificacaoController.java
+++ src/main/java/com/kowalski/casaapi/api/v1/controller/NotificacaoController.java
@@ -3,14 +3,13 @@ package com.kowalski.casaapi.api.v1.controller;
 import com.kowalski.casaapi.business.telegram.MessageCreator;
 import com.kowalski.casaapi.config.telegram.MyGenericBot;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/v1/notificacao", consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/v1/notificacao")
 public class NotificacaoController {
 
     private final MyGenericBot myGenericBot;
@@ -20,12 +19,12 @@ public class NotificacaoController {
     }
 
     @Value("${telegram.chatid}")
-    private Long chatId;
+    private String chatId;
 
     @PostMapping
     public void notificar(@RequestParam String notificacao){
         try {
-            myGenericBot.execute(MessageCreator.createMessage(chatId, notificacao, false));
+            myGenericBot.execute(MessageCreator.createMessage(Long.parseLong(chatId), notificacao, false));
         } catch (Exception e) {
             e.printStackTrace();
         }
